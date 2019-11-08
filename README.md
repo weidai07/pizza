88aa1e85eabd58c3daf4f4c736e8f37ba9c4f907
Wei Dai <wei@>
2019-11-08 14:53:07 -0800

    continue to do more research

---
+++ README.md
@@ -0,0 +1,19 @@
+# _Pizza_
+
+#### _By Wei Dai_
+
+## Description
+
+Order pizza through this website.
+
+## Setup
+
+* _Select a Topping_
+
+* _Select a Size_
+
+* _Place Order_
+
+### Legal
+
+Copyright @ WD19
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function Pizza (toppings, size){
   this.size = size;
 }
 
-Pizza.prototype.cost = function(toppings, size) {
+Pizza.prototype.cost = function() {
   var price = 0
   if (this.toppings === "meat" && this.size === "small") {
     return price = 7;
@@ -17,65 +17,17 @@ Pizza.prototype.cost = function(toppings, size) {
     return false;
   }
 }
-  // return this.totalAmt += this.toppings + this.size;
 
 
-// Account.prototype.total = function(toppings, size){
-//   this.totalAmt += toppings;
-//   this.totalAmt += size;
-//   return this.totalAmt;
-// }
-
-
-
-
-$(document).ready(function(){
-  $("form#formOne").submit(function(event){
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
     event.preventDefault();
 
-      var inputToppings = $("input:radio[name=toppings]:checked").val();
-      var inputSize = $("input:radio[name=size]:checked").val();
+      var inputToppings = $("input:radio[name=toppings1]:checked").val();
+      var inputSize = $("input:radio[name=size1]:checked").val();
       var userInput = new Pizza (inputToppings, inputSize);
-      var result = userInput.cost;
+      var result = userInput.cost();
 
       $(".total1").text(result).show();
-  // $("form#formTwo").submit(function(event){
-  //   event.preventDefault();
-
   });
 });
-
-// this.totalAmt=0
-
-//     var inputMovie = $("#movieID").val();
-//     var inputTime = $("#timeID").val();
-//     var inputAge = $("input:radio[name=age]:checked").val();
-//
-//     var theater = new Theater(inputMovie,inputTime,inputAge);
-//       var result = "Your price is $"+theater.priceCalc()+".00";
-//
-// $("#resultBox").text(result).show();
-
-// $(document).ready(function() {
-//   var person1;
-//   $("form#formOne").submit(function(event){
-//     event.preventDefault();
-//
-//     var userName = $("#name").val();
-//     var dep1 = parseFloat($("#first-deposit").val());
-//     person1 = new Account (userName, dep1);
-//
-//     $(".total1").text(person1.initialDeposit());
-//
-//   });
-//   $("form#formTwo").submit(function(event){
-//     event.preventDefault();
-//
-//     var moneyIn = parseFloat($("#deposit").val());
-//     var moneyOut = parseFloat($("#withdrawal").val());
-//
-//
-//     $(".total1").text(person1.total(moneyIn, moneyOut));
-//
-//   });
-// });
